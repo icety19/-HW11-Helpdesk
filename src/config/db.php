b4ae2e3cf480911080b2f2896aa795b8effda7f2
67703083 ณัฏฐิณิชา ทองกริ่ม <student@spu.ac.th>
2026-03-22 18:26:50 +0700

    [67703083] [ณัฏฐิณิชา ทองกริ่ม] - โครงสร้างพื้นฐาน Docker และหน้าฟอร์ม index.php เสร็จสมบูรณ์

---
+++ src/config/db.php
@@ -0,0 +1,14 @@
+<?php
+// 👨💻 พัฒนาโดย: 67703083 ณัฏฐิณิชา ทองกริ่ม
+$host = 'db';
+$dbname = 'helpdesk_db'; // เดี๋ยวนาย B จะเป็นคนไปสร้างก้อนนี้เอง
+$username = 'dev_user';
+$password = 'dev_password';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "การเชื่อมต่อฐานข้อมูลล้มเหลว: " . $e->getMessage();
+}
+?>
